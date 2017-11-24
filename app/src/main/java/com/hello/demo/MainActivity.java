7138b48b64dc641262eabc0d1d678a361d755d1b
yangzhuoxun <zhuoxun.yang@gometech.com.cn>
2017-11-24 10:19:53 +0800

    [feat]:aes encrypt and decrypt

--- app/src/main/java/com/hello/demo/MainActivity.java
+++ app/src/main/java/com/hello/demo/MainActivity.java
@@ -8,25 +8,33 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-        private TextView text;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-                text = (TextView) findViewById(R.id.text);
-	}
+    private TextView text;
 
-        public void addBigInt(View v) {
-                byte[] src = {1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+    }
+
+    public void addBigInt(View v) {
+        byte[] src = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4,};
 //                byte[] result = NativeAuthenticatorKernel.bigIntAdd();
-                byte[] result = NativeAuthenticatorKernel.invokeCmd(1,src, 16);
-                StringBuilder res = new StringBuilder();
+        int length = src.length;
+        byte[] encrypted = NativeAuthenticatorKernel.invokeCmd(1, src, length);
+        StringBuilder res = new StringBuilder();
+        Log.d("uTClient", encrypted.length + "");
+        for (byte b : encrypted) {
+            res.append(Integer.toHexString(b & 0xFF));
+        }
+        text.setText(res.toString());
 
-                Log.d("uTClient", result.length + "");
-                for(byte b : result)
-                {
-                    res.append(Integer.toHexString(b & 0xFF));
-                }
-                text.setText(res.toString());
-	}
+        res.append("\n");
+        byte[] decrypted = NativeAuthenticatorKernel.invokeCmd(2, encrypted, length);
+        Log.d("uTClient", decrypted.length + "");
+        for (byte b : decrypted) {
+            res.append(Integer.toHexString(b & 0xFF));
+        }
+        text.setText(res.toString());
+    }
 }
