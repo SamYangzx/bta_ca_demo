55e27f8ce69c04b188cd4fec32e31a5cd49c7659
yangzhuoxun <zhuoxun.yang@gometech.com.cn>
2017-11-28 14:14:39 +0800

    BeanpodTEEClient is ok

--- app/src/main/java/com/gome/teetest/MainActivity.java
+++ app/src/main/java/com/gome/teetest/MainActivity.java
@@ -1,10 +1,11 @@
-package com.hello.demo;
+package com.gome.teetest;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import  com.gome.teetest.BeanpodTEEClient.*;
 
 public class MainActivity extends Activity {
 
@@ -17,11 +18,11 @@ public class MainActivity extends Activity {
         text = (TextView) findViewById(R.id.text);
     }
 
-    public void addBigInt(View v) {
+    public void encrypt(View v) {
         byte[] src = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4,};
-//                byte[] result = NativeAuthenticatorKernel.bigIntAdd();
+//                byte[] result = BeanpodTEEClient.bigIntAdd();
         int length = src.length;
-        byte[] encrypted = NativeAuthenticatorKernel.invokeCmd(1, src, length);
+        byte[] encrypted = BeanpodTEEClient.invokeCmd(BeanpodTEEClient.ENCRYPT, src, length);
         StringBuilder res = new StringBuilder();
         Log.d("uTClient", encrypted.length + "");
         for (byte b : encrypted) {
@@ -30,7 +31,7 @@ public class MainActivity extends Activity {
         text.setText(res.toString());
 
         res.append("\n");
-        byte[] decrypted = NativeAuthenticatorKernel.invokeCmd(2, encrypted, length);
+        byte[] decrypted = TEEClient.invokeCmd(BeanpodTEEClient.DECRYPT, encrypted, length);
         Log.d("uTClient", decrypted.length + "");
         for (byte b : decrypted) {
             res.append(Integer.toHexString(b & 0xFF));
---
+++ app/src/main/java/com/gome/teetest/TEEClient.java
@@ -0,0 +1,19 @@
+package com.gome.teetest;
+
+/**
+ * Created by jiamei on 6/26/15.
+ */
+public class TEEClient {
+    public static final int ENCRYPT = 0x10003;
+    public static final int DECRYPT = 0x10004;
+
+    static {
+        System.loadLibrary("mtee");
+        System.loadLibrary("teeclientsoft");
+    }
+
+//    public static native byte[] bigIntAdd();
+
+//    public static native byte[] invokeCmd(int cmd, byte[] input, int length);
+    public static native byte[] invokeCmd(int cmd, byte[] input, int length);
+}
